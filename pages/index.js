a62e81135c223eebfdd651bfa7dc4a3db1ef3b17
John William Davis <jwdavisdeveloper@gmail.com>
2021-10-08 13:40:39 -0700

    Playing around with pics and spacing - intial steps

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,30 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Heading, Box } from "@dracula/dracula-ui";
+import { Heading, Box, Paragraph } from "@dracula/dracula-ui";
 
 export default function Home() {
   return (
-    <Box p='lg'>
-      <Heading>Working with Dracula UI here 😱</Heading>
-    </Box>
+    <div className='page-wrapper'>
+      <Box p='lg'>
+        <Heading size='2xl'>The Walking Dead</Heading>
+        <Heading size='xl'>Season 11 😱</Heading>
+        <Paragraph color='purpleCyan'>A loving fan site!</Paragraph>
+        <Image
+          src='/title1.jpg'
+          width={200}
+          height={200}
+          alt='Walking Dead Title'
+          quality={100}
+        />
+      </Box>
+      <Box>
+        <Image
+          src='/negan1.jpg'
+          width={400}
+          height={400}
+          alt='Character of Negan - The Walking Dead'
+        />
+      </Box>
+    </div>
   );
 }
