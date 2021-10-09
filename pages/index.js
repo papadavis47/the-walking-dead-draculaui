1e5017a58d952c07239c99d6783760a22c703c89
John William Davis <jwdavisdeveloper@gmail.com>
2021-10-09 00:52:41 -0700

    Switch background to pure black, change to using css modules with draculaui css

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,31 @@
 import Head from "next/head";
 import Image from "next/image";
+import styles from "../styles/Home.module.css";
 import { Heading, Box, Paragraph } from "@dracula/dracula-ui";
 
 export default function Home() {
   return (
-    <div className='page-wrapper'>
+    <div className={styles.wrapper}>
       <Box p='lg'>
         <Heading size='2xl'>The Walking Dead</Heading>
-        <Heading size='xl'>Season 11 😱</Heading>
-        <Paragraph color='purpleCyan'>A loving fan site!</Paragraph>
-        <Image
-          src='/title1.jpg'
-          width={200}
-          height={200}
-          alt='Walking Dead Title'
-          quality={100}
-        />
+        <Heading color='purpleCyan' size='xl'>
+          A Loving Fansite for Season 11!
+        </Heading>
       </Box>
-      <Box>
-        <Image
-          src='/negan1.jpg'
-          width={400}
-          height={400}
-          alt='Character of Negan - The Walking Dead'
-        />
-      </Box>
+      <Image
+        src='/negan1.jpg'
+        width={400}
+        height={400}
+        alt='Character of Negan - The Walking Dead'
+      />
+      <Image
+        src='/title1.jpg'
+        width={200}
+        height={200}
+        alt='Walking Dead Title'
+        quality={100}
+      />
+      <Box></Box>
     </div>
   );
 }
